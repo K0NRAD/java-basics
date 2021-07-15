f1336910c64e3e5bb291281bc716882db1cb3384
Hauke <konrad.hauke@mail.schwarz>
2021-07-15 15:41:29 +0200

    add step2 code

--- src/main/java/sit/learninghub/App.java
+++ src/main/java/sit/learninghub/App.java
@@ -1,5 +1,25 @@
 package sit.learninghub;
 
-public class App {
+/*
+Aufgabe
 
+Schreibe ein Programm das als Eingabe die Integer Werte `numberA` und `numberB` über die Kommandozeile einliest,
+die beiden Werte addiert und die Summe auf der Konsole Ausgibt.
+
+Tip
+
+Für die Eingabe von der Konsole verwende dir Klasse Scanner und deren Funktion nextInt() um die Werte einzulesen.
+
+    Scanner scanner = new Scanner(System.in);
+    int numberA = scanner.nextInt();
+
+Um Text auf der Konsole auszugeben verwende die Funktion println() der Klasse Syste.out
+
+    System.out.println(result);
+
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
 }
